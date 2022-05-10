2169c88d704efbc39e9c21cdda4a86f4aea87af0
olegweremyew <olegweremey1994@mail.ru>
2022-05-10 14:03:45 +0300

    added eslint, prettier and other lib

---
+++ src/App/App.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+import './App.css';
+import { ReturnComponentType } from '../types/ReturnComponentType';
+
+const App = (): ReturnComponentType => <div className="App">fghgfhghjgg</div>;
+
+export default App;
